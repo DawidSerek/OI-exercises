0b32d8ec24a56bd5db1c68c69cc66cde678d192a
DawidSerek <dawid.serek@gmail.com>
2021-04-07 17:30:52 +0200

    szablon 2.95/3
    i hate this shit, im out
    90 % smh

--- szablon/szablon.cpp
+++ szablon/szablon.cpp
@@ -1,58 +1,69 @@
 #include<iostream>
-#include<algorithm>
-
 using namespace std;
-const int _maxN = 100;
 
-int KMP[_maxN + 1];
-bool can[_maxN + 1];
+const int _maxN = 500000;
 
-bool isOk(string inp, int can)
+int KMP[_maxN * 2 + 2];
+bool kmpCan[_maxN + 1];
+string inp;
+
+void KMPf( string txt )
 {
-    return false;
+    int n = txt.size();
+    int p = 0;
+    for(int i = 2; i < n; i++)
+    {
+        while( p > 0 && txt[p + 1] != txt[i] )
+            p = KMP[p];
+        if( txt[p + 1] == txt[i] )
+            p++;
+        KMP[i] = p;
+    }
+}
+
+bool isOk(int can)
+{
+    string candidate = "";
+    for(int i = 0; i <= can; i++)
+        candidate += inp[i];
+    string outStr = candidate + inp;
+    KMPf( outStr );
+
+    int limit = can;
+    for( int i = can + 2; i < outStr.size(); i++ )
+    {
+        limit--;
+        if( KMP[i] == can )
+            limit = can;
+        if( limit == 0 )
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
-    string inp;
     cin >> inp;
-    inp = '#' + inp;
-    int n = inp.size();
-
-    int p = 0;
-    for(int i = 2; i < inp.size(); i++)
+    inp = "#" + inp;
+    
+    KMPf( inp );
+    int p = inp.size() - 1;
+    while( p != 0 )
     {
-        while( p > 0 && inp[p + 1] != inp[i]  )
-            p = KMP[p];
-        if( inp[p + 1] == inp[i] )
-            p++;
-        KMP[i] = p;
-    }
-
-    can[n] = true;
-    while( KMP[p] != 0 )
-    {
+        kmpCan[p] = true;
         p = KMP[p];
-        can[p] = true;
     }
-
-    for(int i = 1; i <= n; i++)
+    
+    for(int i = 1; i <= inp.size(); i++)
     {
-        if( can[i] )
-            if( isOk(inp, i) )
+        if( kmpCan[i] )
+            if( isOk(i) )
             {
                 cout << i;
                 break;
             }
             else
-                for( int j = i + 1; j <= 2 * i || j <= n; j++ )
-                    can[j] = false;
+                i = i * 2;
     }
-        
-
-    // for(int i = 1 ; i < inp.size(); i++)
-    //     cout << KMP[i] /*<< inp[i]/**/ << " ";
-
     return 0;
 }
-
