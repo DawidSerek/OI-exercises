5562a888fe3c7731dc3fb1e11ac6cf86198cb2cd
DawidSerek <dawid.serek@gmail.com>
2021-04-07 13:47:53 +0200

    szablon p 1/2

--- szablon/szablon.cpp
+++ szablon/szablon.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 const int _maxN = 100;
 
-int KMP[_maxN];
+int KMP[_maxN + 1];
+bool can[_maxN + 1];
+
+bool isOk(string inp, int can)
+{
+    return false;
+}
 
 int main()
 {
     string inp;
     cin >> inp;
     inp = '#' + inp;
+    int n = inp.size();
 
     int p = 0;
     for(int i = 2; i < inp.size(); i++)
@@ -22,8 +29,29 @@ int main()
         KMP[i] = p;
     }
 
-    for(int i = 1 ; i < inp.size(); i++)
-        cout << KMP[i] /*<< inp[i]/**/ << " ";
+    can[n] = true;
+    while( KMP[p] != 0 )
+    {
+        p = KMP[p];
+        can[p] = true;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if( can[i] )
+            if( isOk(inp, i) )
+            {
+                cout << i;
+                break;
+            }
+            else
+                for( int j = i + 1; j <= 2 * i || j <= n; j++ )
+                    can[j] = false;
+    }
+        
+
+    // for(int i = 1 ; i < inp.size(); i++)
+    //     cout << KMP[i] /*<< inp[i]/**/ << " ";
 
     return 0;
 }
