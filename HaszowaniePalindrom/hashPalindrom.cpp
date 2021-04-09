982759ad25c821dd0bc32259d4050aed1d924a6e
DawidSerek <dawid.serek@gmail.com>
2021-04-09 16:32:21 +0200

    resolved struct size limit issue in palindrom

--- HaszowaniePalindrom/hashPalindrom.cpp
+++ HaszowaniePalindrom/hashPalindrom.cpp
@@ -2,79 +2,70 @@
 using namespace std;
 
 typedef long long ll;
-const int _maxN = 100000;
+const int _maxN = 500000;
+const ll _sP = 31;
+const ll _lP = 1000000003;
 
-struct hashS
+ll hashT[_maxN][2];
+ll powT[_maxN];
+string inp;
+bool isP;
+
+void iniPow()
 {
-    const ll _sP = 31;
-    const ll _lP = 1000000003;
 
-    ll hashT[_maxN];
-    ll pow[_maxN];
-    string inp;
-    bool isP;
+    powT[0] = 1;
+    for(int i = 1; i < _maxN; i++)
+        powT[i] = (powT[i-1] * _sP) % _lP;
+}
 
-    void iniPow()
+void iniS(string txt)
+{
+    inp = txt;
+    int n = inp.size() - 1;
+    hashT[n][0] = int( inp[n] );
+    for(int i = n - 1; i >= 0; i--)
+        hashT[i][0] = ( hashT[i + 1][0] * _sP + int(inp[i]) ) % _lP;
+    
+}
+void iniP(string txt)
+{
+    inp = txt;
+    int n = inp.size();
+    hashT[0][1] = int( inp[0] );
+    for(int i = 1; i < n; i++)
+        hashT[i][1] = ( hashT[i - 1][1] * _sP + int(inp[i]) ) % _lP;
+}
+
+int findR(int a, int b, int isP)
+{
+    if( !isP )
     {
-       
-        pow[0] = 1;
-        for(int i = 1; i < _maxN; i++)
-            pow[i] = (pow[i-1] * _sP) % _lP;
+        ll rmv = (hashT[b + 1][0] * powT[b + 1 - a]) % _lP;
+        if( b + 1 >= inp.size() ) rmv = 0;
+        ll out = hashT[a][0] - rmv;
+        if( out < 0 )
+            out += _lP;
+        return out;
     }
-
-    void iniS(string txt)
+    else
     {
-        inp = txt;
-        isP = false;
-        int n = inp.size() - 1;
-        hashT[n] = int( inp[n] );
-        for(int i = n - 1; i >= 0; i--)
-            hashT[i] = ( hashT[i + 1] * _sP + int(inp[i]) ) % _lP;
-        iniPow();
+        ll rmv = 0;
+        if( a - 1 >= 0 ) rmv = (hashT[a - 1][1] * powT[b + 1 - a]) % _lP;
+        ll out = hashT[b][1] - rmv;
+        if( out < 0 )
+            out += _lP;
+        return out;
     }
-    void iniP(string txt)
-    {
-        inp = txt;
-        isP = true;
-        int n = inp.size();
-        hashT[0] = int( inp[0] );
-        for(int i = 1; i < n; i++)
-            hashT[i] = ( hashT[i - 1] * _sP + int(inp[i]) ) % _lP;
-        iniPow();
-    }
+}
 
-    int findR(int a, int b)
-    {
-        if( !isP )
-        {
-            ll rmv = (hashT[b + 1] * pow[b + 1 - a]) % _lP;
-            if( b + 1 >= inp.size() ) rmv = 0;
-            ll out = hashT[a] - rmv;
-            if( out < 0 )
-                out += _lP;
-            return out;
-        }
-        else
-        {
-            ll rmv = 0;
-            if( a - 1 >= 0 ) rmv = (hashT[a - 1] * pow[b + 1 - a]) % _lP;
-            ll out = hashT[b] - rmv;
-            if( out < 0 )
-                out += _lP;
-            return out;
-        }
-    }
-};
-
-
-
-int binS(int i, bool isOdd, string inp, struct hashS *pre, struct hashS *suf)
+int binS(int i, bool isOdd, string inp)
 {
     int start = 1, end = min( i, int(inp.size() - i) ), mid, tOut = 0;
     while( start <= end )
     {
         mid = (start + end) / 2;
-        if( pre->findR( i - mid, i - 1 + isOdd) == suf->findR(i, i + mid - 1 + isOdd) )
+        if( findR( i - mid, i - 1 + isOdd, 1) == findR(i, i + mid - 1 + isOdd, 0) )
         {
             tOut = mid;
             start = mid + 1;
@@ -90,16 +81,15 @@ int main()
     string inp;
     cin >> inp;
 
-    hashS pre, suf;
-    pre.iniP(inp);
-    suf.iniS(inp);
+    iniPow();
+    iniP(inp);
+    iniS(inp);
 
-    int out = 0;
-    /*
+    ll out = 0;
     for(int i = 1; i < inp.size(); i++)
     {
-        out += binS(i, 1, inp, &pre, &suf);
-        out += binS(i, 0, inp, &pre, &suf);
-    }/**/
+        out += binS(i, 1, inp);
+        out += binS(i, 0, inp);
+    }
     cout << out;
 }
