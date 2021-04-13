c47fd4a0d59fafedaa58894dcff88a82b7034ef9
DawidSerek <dawid.serek@gmail.com>
2021-04-13 12:28:53 +0200

    5napisow -> O(n log n log n)

--- 5Napisow/5napisow.cpp
+++ 5Napisow/5napisow.cpp
@@ -1,77 +1,77 @@
 #include<iostream>
-#include<algorithm>
+#include<vector>
+#include<map>
 using namespace std;
 
 typedef long long ll;
 
-struct hashStruct
+const int _maxN = 100001;
+const int _sP = 31;
+const int _lP = 1000000003;
+
+ll hashT[_maxN][5];
+ll powT[_maxN];
+
+void hashF(string data, int index)
 {
-    const int _maxN = 5000;
-    const int _sP = 31;
-    const int _lP = 1000000003;
-    ll hashT[5000];
-    ll pow[5000];
+    int n = data.size();
+    hashT[n-1][index] = int( data[n-1] );
+    for(int i = n - 2; i >= 0; i--)
+        hashT[i][index] = (hashT[i + 1][index] * _sP + int( data[i] ) ) % _lP;
+}
+int findR(int a, int b, int index)
+{
+    ll out = hashT[a][index];
+    ll rmv = ( hashT[b + 1][index] * powT[b-a + 1] ) %_lP;
+    out -= rmv;
+    if(out < 0) return out + _lP;
+    return out;
+}
 
-    void ini(string inp)
+map< int, int > isHash;
+string inp[5];
+bool isOk(int mid)
+{
+    if( mid == 0 ) return true;
+    for(int i = 0; i < 5; i++)
     {
-        int n = inp.size();
-
-        pow[0] = 1;
-        for(int i = 1; i < n; i++)
-            pow[i] = (pow[i-1] * _sP) % _lP;
-
-        hashT[n-1] = int( inp[n-1] );
-        for( int i = n - 2; i >= 0; i-- )
-            hashT[i] = ( hashT[i + 1] * _sP + int(inp[i]) ) % _lP;
+        int temp = inp[i].size() - mid + 1;
+        for(int j = 0; j < temp; j++)
+        {
+            ll nowHash = findR(j, j + mid - 1, i);
+            if( isHash[nowHash] == i )
+                isHash[nowHash]++;
+            if( isHash[nowHash] == 5 )
+                return true;
+        }
     }
-
-    ll hRange(int a, int b)
-    {
-        ll out = hashT[a];
-        ll rmv = (hashT[b+1] * pow[ b-a + 1 ]) % _lP;
-        out -= rmv;
-
-        if( out < 0 )
-            return out + _lP;
-        return out;
-    }
-
-};
+    isHash.clear();
+    return false;     
+}
 
 int main()
 {
-    string inp[5];
-    for(int i = 0; i < 5; i++)
+    powT[0] = 1;
+    for(int i = 1; i < _maxN; i++)
+        powT[i] = (powT[i - 1] * _sP) % _lP;
+
+    for(int i = 0; i < 5; i ++)
         cin >> inp[i];
+    for(int i = 0; i < 5; i++) 
+        hashF(inp[i], i);
 
-    hashStruct hashStr[5];
-    for(int i = 0; i < 5; i++)
-        hashStr[i].ini(inp[i]);
-    
-    int n = inp[0].size(), minN = inp[0].size();
-    for(int i = 1; i < 5; i++)
-        minN = min( minN, int(inp[i].size()) );
+    int start = 0, end = inp[0].size(), mid, out;
+    while( start <= end )
+    {
+        mid = (start + end) / 2;
 
-    int out = 0;
-    for(int i = 0; i < n; i++)
-        for(int j = i; j < n; j++)
+        if( isOk(mid) )
         {
-            ll exHash = hashStr[0].hRange(i, j);
-            int range = j-i;
-            bool isOk = true;
-            for(int k = 1; k < 5; k++)
-            {
-                bool isItOk = false;
-                for(int l = 0; l + range < inp[k].size(); l++)
-                    if( hashStr[k].hRange(l, l + range) == exHash )
-                        isItOk = true;
-                if( !isItOk )
-                    isOk = false;
-            }
-            if(isOk)
-                out = max(out, range + 1);
+            start = mid + 1;
+            out = mid;
         }
-        
+        else
+            end = mid - 1;
+    }
     cout << out;
-    return 0;
 }
