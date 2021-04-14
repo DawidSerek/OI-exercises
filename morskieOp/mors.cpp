38e7924411c65a9aaed33825517aeaf7a447682f
DawidSerek <dawid.serek@gmail.com>
2021-04-14 15:21:30 +0200

    Morskie opowieści
    added morskie opowieści; code seems pretty bad tho, so im planning to totally remake it. doesnt go with tests very well.  helped to understand problem more tho.

--- morskieOp/mors.cpp
+++ morskieOp/mors.cpp
@@ -1,115 +1,110 @@
-#include <iostream>
-#include<vector>
+#include<iostream>
 #include<map>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-const int _maxN = 6000;
+const int _maxN = 20;
+const int _maxK = 1000000;
 
-namespace maps
-{
-    map < pair<int, int>, bool > vis;
-    map < pair<int, int>, int > inp;
-    map < pair<int, int>, pair<int, int> > out;
-}
+vector <int> nei[_maxN + 1];
+bool vis[_maxN + 1][2];
+int path[_maxN + 1][2];
 
-bool vis[_maxN][2];
-int clr()
-{
-    for(int i = 0; i < _maxN; i++)
-    {
-        vis[i][0] = false;
-        vis[i][1] = false;
-    }
-}
+pair<int, pair<int, int> > inpQ[_maxK]; 
+int inp[_maxN][3];
+map < pair<int, int>, pair<int, int> > out;
 
-vector <int> sas[_maxN + 1];
-vector < pair<int, int> > q;
-int BFS( int r = 1 )
-{
-    maps::out[ make_pair(r, r) ].first = 0;
-
-    q.push_back( make_pair(r, 0) );
-    vis[r][0] = true;
-    for(int i = 0; i < q.size(); i++)
-    {
-        int p = q[i].first;
-        int dis = q[i].second + 1;
-
-        for(int j = 0; j < sas[p].size(); j++)
-        {
-            int n = sas[p][j];
-            if( !vis[n][dis % 2] )
-            {
-                vis[n][dis % 2] = true;
-                q.push_back( make_pair(n, dis) );
-
-                pair<int, int> tPair = make_pair( min(r, n), max(r, n) );
-                if( maps::vis[tPair] )
-                    if( dis % 2 == 0 )
-                        maps::out[ tPair ].first = dis;
-                    else
-                        maps::out[ tPair ].second = dis;
-            }
-        }
-    }
-}
-
-pair <int, int> inp[_maxN];
 int main()
 {
-    int n, m, z;
-    cin >> n >> m >> z;
+    int n, m, k;
+    cin >> n >> m >> k;
 
     int a, b, c;
     for(int i = 0; i < m; i++)
     {
         cin >> a >> b;
-        sas[a].push_back(b);
-        sas[b].push_back(a);
+        nei[a].push_back(b);
+        nei[b].push_back(a);
     }
 
-    for(int i = 0; i < z; i++)
+    for(int i = 0; i < k; i++)
     {
-        cin >> a >> b >> c;
-        inp[i] = make_pair(a, b);
-        pair<int, int> tPair = make_pair( min(a, b), max(a, b) );
-        maps::inp[ tPair ] = c;
-        maps::vis[ tPair ] = true;
-        maps::out[ tPair ] = make_pair(-1, -1);
+        cin >> inp[i][0] >> inp[i][1] >> inp[i][2];
+        inpQ[i].first = inp[i][0];
+        inpQ[i].second.first = inp[i][1];
+        inpQ[i].second.second = inp[i][2];
+        out[ make_pair(inpQ[i].first, inpQ[i].second.first) ].first = 10000000;
+        out[ make_pair(inpQ[i].first, inpQ[i].second.first) ].second = 10000000;
+    }
+        
+    sort( inpQ, inpQ + k );
+
+    vector <int> sP;
+    sP.push_back( inpQ[0].first );
+    for(int i = 1; i < k; i++)
+        if( inpQ[i].first != sP[ sP.size()-1 ] )
+            sP.push_back( inpQ[i].first );
+    sP.push_back( -1 );
+
+    int pointer = 0;
+    for(int i = 0; i < sP.size() - 1; i++)
+    {
+        vector < pair<int, int> > q; 
+        q.push_back( make_pair( sP[i], 0 ) );
+
+        for(int j = 0; j < q.size(); j++)
+        {
+            int p = q[j].first;
+            int pp = q[j].second;
+
+            for(int l = 0; l < nei[p].size(); l++)
+            {
+                int s = nei[p][l]; 
+                if( !vis[s][ (pp+1)%2 ] )
+                {
+                    vis[s][ (pp+1)%2 ] = true;
+                    path[s][ (pp+1)%2 ] = pp+1;
+                    q.push_back( make_pair(s, pp+1) );
+                }
+            }
+                
+        }
+
+        while( inpQ[pointer].first == sP[i] )
+        {
+            int inpQb = inpQ[pointer].second.first, inpQc = inpQ[pointer].second.second;
+            if( vis[inpQb][inpQc%2] )
+                if( inpQc%2 )
+                    out[ make_pair( sP[i], inpQb ) ].second = path[inpQb][inpQc%2];
+                else
+                    out[ make_pair( sP[i], inpQb ) ].first = path[inpQb][inpQc%2];
+            pointer++;
+        }
+
+        for(int j = 1; j <= _maxN; j++)
+        {
+            vis[j][0] = false;
+            vis[j][1] = false;
+        }
     }
 
-    for(int i = 0; i < n; i++)
-        BFS(i + 1);
-
-    /*
-    for(int i = 0; i < q.size(); i++)
-        cout << q[i].first << " ";
-    cout << endl;
-    for(int i = 0; i < q.size(); i++)
-        cout << q[i].second << " ";
-    cout << endl;
-    for(int i = 0; i < z; i++)
+    for(int i = 0; i < k; i++)
     {
-        pair<int, int> tPair = make_pair( min(inp[i].first, inp[i].second), max(inp[i].first, inp[i].second) );
-        cout << tPair.first << " " << tPair.second << "!" << maps::out[tPair].first << "!" << maps::out[tPair].second << endl;
-    }
-    /**/
-    for(int i = 0; i < z; i++)
-    {
-        pair<int, int> tPair = make_pair( min(inp[i].first, inp[i].second), max(inp[i].first, inp[i].second) );
-        int minEv = maps::out[tPair].first;
-        int minOdd = maps::out[tPair].second;
-        int querry = maps::inp[ tPair ];
-
-        if( querry % 2 == 0 )
-            if( minEv <= querry )
+        a = inp[i][0], b = inp[i][1], c = inp[i][2];
+        if( c%2 == 0 )
+        {
+            if( out[ make_pair(a, b) ].first <= c ) 
                 cout << "TAK" << endl;
             else
                 cout << "NIE" << endl;
+        }
         else
-            if( minOdd <= querry )
+        {
+            if( out[ make_pair(a, b) ].second <= c )
                 cout << "TAK" << endl;
             else
                 cout << "NIE" << endl;
+        }  
     }
 }
